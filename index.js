f95c6a4cd5d1868fc4fb55332e3f59bada65b121
Nikhil Gone <nikhilgone147@gmail.com>
2026-02-11 14:18:33 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("bow bow");
+
+const obj = [
+  {
+    name: "nikhil",
+    age: 21,
+  },
+  { username: "cherry", age: 25 },
+];
+const age = obj.map((eachItem) => {
+  console.log(eachItem.age);
+});
